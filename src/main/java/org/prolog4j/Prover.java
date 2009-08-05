a163d5a21249aaa536fbe3a0d7423df1f27dd691
Miklos Espak <espak.miklos@gmail.com>
2009-08-05 12:43:10 +0000

    Javadoc comments mostly.

--- src/main/java/org/prolog4j/Prover.java
+++ src/main/java/org/prolog4j/Prover.java
@@ -5,94 +5,177 @@ import alice.tuprolog.InvalidTheoryException;
 import alice.tuprolog.Prolog;
 import alice.tuprolog.Theory;
 
+/**
+ * Represents a Prolog knowledge base and provides methods for solving queries
+ * on it. The prover itself is not responsible for processing the solutions.
+ * 
+ * @see org.prolog4j.Solution
+ */
 public class Prover {
 
-    public static final Prover GLOBAL = new CachingProver();
-    // static {
-    // GLOBAL.loadLibrary("japlo.lib.OOLibrary");
-    // try {
-    // GLOBAL.engine.loadLibrary(new MetaLibrary(GLOBAL.engine));
-    // } catch (InvalidLibraryException e1) {
-    // e1.printStackTrace();
-    // }
-    // GLOBAL.engine.addWarningListener(new WarningListener() {
-    // public void onWarning(WarningEvent e) {
-    // System.out.println(e.getMsg());
-    // }
-    // });
-    // GLOBAL.engine.addOutputListener(new OutputListener() {
-    // public void onOutput(alice.tuprolog.event.OutputEvent e) {
-    // System.out.println(e.getMsg());
-    // };
-    // });
-    // }
+	// public static final Prover GLOBAL = new CachingProver();
+	public static final Prover GLOBAL = new Prover();
 
-    public final Prolog engine;
+	// static {
+	// GLOBAL.engine.addWarningListener(new WarningListener() {
+	// public void onWarning(WarningEvent e) {
+	// System.out.println(e.getMsg());
+	// }
+	// });
+	// GLOBAL.engine.addOutputListener(new OutputListener() {
+	// public void onOutput(alice.tuprolog.event.OutputEvent e) {
+	// System.out.println(e.getMsg());
+	// };
+	// });
+	// }
 
-    public Prover() {
-        engine = new Prolog();
-    }
+	/**
+	 * Returns the global prover.
+	 * 
+	 * @return the global prover
+	 */
+	@Deprecated
+	public static Prover get() {
+		return GLOBAL;
+	}
 
-    public void loadLibrary(String className) {
-        try {
-            engine.loadLibrary(className);
-        } catch (InvalidLibraryException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	protected final Prolog engine;
 
-    public <A> Solution<A> solve(String goal) {
-        System.out.println("Prover.solve() " + goal);
-        // return new Solution2<A>(engine, Solution2.goalTerms(goal, 0));
-        return new Solution<A>(engine, goal);
-    }
+	public Prover() {
+		engine = new Prolog();
+	}
 
-    public <A> Solution<A> solve(String goal, int inputArgs, Object... actualArgs) {
-        return new Solution<A>(engine, Solution.goalTerms(goal, inputArgs), actualArgs);
-    }
+	public void loadLibrary(String className) {
+		try {
+			engine.loadLibrary(className);
+		} catch (InvalidLibraryException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public <A> Solution<A> solve(String goal, Object... actualArgs) {
-        System.out.println("Prover.solve() 2");
-        return new Solution<A>(engine, Solution.goalTerms(goal, (1 << actualArgs.length) - 1), actualArgs);
-        // return new Solution2<A>(engine, goal, actualArgs);
-    }
+	/**
+	 * Solves a Prolog goal and returns an object using which the individual
+	 * solutions can be iterated over.
+	 * 
+	 * @param <A>
+	 *            the type of an element of the solutions
+	 * @param goal
+	 *            the Prolog goal
+	 * @return an object for traversing the solutions
+	 */
+	public <A> Solution<A> solve(String goal) {
+		System.out.println("Prover.solve() " + goal);
+		// return new Solution2<A>(engine, Solution2.goalTerms(goal, 0));
+		return new Solution<A>(engine, goal);
+	}
 
-    // public <A> Solution<A> solve(String goal, Map<String, Object> args) {
-    // int argNo = args.size();
-    // String[] inputArgs = args.keySet().toArray(new String[argNo]);
-    // Arrays.sort(inputArgs);
-    // Object[] actualArgs = new Object[argNo];
-    // for (int i = 0; i < argNo; ++i)
-    // actualArgs[i] = args.get(inputArgs[i]);
-    // return new Solution<A>(engine, Solution.goalTerms(goal, inputArgs),
-    // actualArgs);
-    // }
+	/**
+	 * Solves a Prolog goal and returns an object using which the individual
+	 * solutions can be iterated over. The goal must be a single compound term
+	 * whose arguments are variables. The inputArgs argument is a bit vector
+	 * that show which arguments of the goal term are regarded as inputs.
+	 * 
+	 * @param <A>
+	 *            the type of an element of the solutions
+	 * @param goal
+	 *            the Prolog goal
+	 * @param inputArgs
+	 *            denotes the input arguments of the goal as a bit vector
+	 * @param actualArgs
+	 *            the actual arguments of the goal
+	 * @return an object for traversing the solutions
+	 */
+	@Deprecated
+	public <A> Solution<A> solve(String goal, int inputArgs,
+			Object... actualArgs) {
+		return new Solution<A>(engine, Solution.goalTerms(goal, inputArgs),
+				actualArgs);
+	}
 
-    public <A> Solution<A> solve(String goal, String[] inputArgs, Object[] actualArgs) {
-        return new Solution<A>(engine, Solution.goalTerms(goal, inputArgs), actualArgs);
-    }
+	/**
+	 * Solves a Prolog goal and returns an object using which the individual
+	 * solutions can be iterated over. The goal must be a single compound term
+	 * whose arguments are variables. The arity of the goal term has to equal
+	 * the number of actual arguments. The actual arguments will be bound to the
+	 * variables before solving the goal.
+	 * 
+	 * @param <A>
+	 *            the type of an element of the solutions
+	 * @param goal
+	 *            the Prolog goal
+	 * @param actualArgs
+	 *            the actual arguments of the goal
+	 * @return an object for traversing the solutions
+	 */
+	public <A> Solution<A> solve(String goal, Object... actualArgs) {
+		return new Solution<A>(engine, Solution.goalTerms(goal,
+				(1 << actualArgs.length) - 1), actualArgs);
+		// return new Solution2<A>(engine, goal, actualArgs);
+	}
 
-    public void addTheory(String theory) {
-        try {
-            engine.addTheory(new Theory(theory));
-        } catch (InvalidTheoryException e) {
-            e.printStackTrace();
-        }
-    }
+	// public <A> Solution<A> solve(String goal, Map<String, Object> args) {
+	// int argNo = args.size();
+	// String[] inputArgs = args.keySet().toArray(new String[argNo]);
+	// Arrays.sort(inputArgs);
+	// Object[] actualArgs = new Object[argNo];
+	// for (int i = 0; i < argNo; ++i)
+	// actualArgs[i] = args.get(inputArgs[i]);
+	// return new Solution<A>(engine, Solution.goalTerms(goal, inputArgs),
+	// actualArgs);
+	// }
 
-    public static Prover get() {
-        return GLOBAL;
-    }
+	/**
+	 * Solves a Prolog goal and returns an object using which the individual
+	 * solutions can be iterated over. The goal has to contain variables with
+	 * the names specified by the second argument. The actual arguments will be
+	 * bound to these variables before solving the goal.
+	 * 
+	 * @param <A>
+	 *            the type of an element of the solutions
+	 * @param goal
+	 *            the Prolog goal
+	 * @param inputArgs
+	 *            the names of the input variables of the goal
+	 * @param actualArgs
+	 *            the actual arguments of the goal
+	 * @return an object for traversing the solutions
+	 */
+	public <A> Solution<A> solve(String goal, String[] inputArgs,
+			Object[] actualArgs) {
+		return new Solution<A>(engine, Solution.goalTerms(goal, inputArgs),
+				actualArgs);
+	}
 
-    public void addTheory(String... theories) {
-        StringBuilder sb = new StringBuilder();
-        for (String theory : theories)
-            sb.append(theory).append('\n');
-        try {
-            engine.addTheory(new Theory(sb.toString()));
-        } catch (InvalidTheoryException e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * Adds a Prolog theory to the knowledge base.
+	 * 
+	 * @param theory
+	 *            the Prolog theory
+	 */
+	public void addTheory(String theory) {
+		try {
+			engine.addTheory(new Theory(theory));
+		} catch (InvalidTheoryException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Adds a Prolog theory to the knowledge base. The elements of the arguments
+	 * must represent individual Prolog facts and rules.
+	 * 
+	 * @param theory
+	 *            the Prolog theory
+	 */
+	public void addTheory(String... theory) {
+		StringBuilder sb = new StringBuilder();
+		for (String factOrRule : theory)
+			sb.append(factOrRule).append('\n');
+		try {
+			engine.addTheory(new Theory(sb.toString()));
+		} catch (InvalidTheoryException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- src/main/java/org/prolog4j/SolutionIterator.java
+++ src/main/java/org/prolog4j/SolutionIterator.java
@@ -2,10 +2,42 @@ package org.prolog4j;
 
 import java.util.Iterator;
 
+/**
+ * The <tt>SolutionIterator</tt> interface servers for traversing the solutions
+ * of a Prolog query. In a solution several variables can get bound. <tt>S</tt>
+ * is the type of the values of the variable that the is of special interest.
+ * Its values are returned by <tt>next()</tt>. The values of the other variables
+ * can be accessed by the <tt>get()</tt> methods.
+ * 
+ * @param <S>
+ *            the type of the values of the variable that is of special interest
+ */
 public interface SolutionIterator<S> extends Iterator<S> {
 
-    <A> A get(String argName);
+	/**
+	 * Returns the value of the variable bound by the current solution.
+	 * 
+	 * @param <A>
+	 *            the type of the value
+	 * @param variable
+	 *            the name of the variable
+	 * @return the value of the variable in the current solution
+	 */
+	<A> A get(String variable);
 
-    <A> A get(String argName, Class<A> type);
+	/**
+	 * Returns the value of the variable bound by the current solution. The
+	 * required type of the value can be specified explicitly. This is useful
+	 * when not the default type is desired.
+	 * 
+	 * @param <A>
+	 *            the type of the value
+	 * @param variable
+	 *            the name of the variable
+	 * @param type
+	 *            the type which the value should be converted to
+	 * @return the value of the variable in the current solution
+	 */
+	<A> A get(String variable, Class<A> type);
 
 }
